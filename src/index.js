ef23461ff160d9126bd0c75bb1417a84af332df4
memotail <memotail@163.com>
2017-09-26 17:36:59 +0800

    add hot replace

--- src/index.js
+++ src/index.js
@@ -4,5 +4,27 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { AppContainer } from 'react-hot-loader';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const render = Component => {
+  if (document.getElementById('root')) {
+    ReactDOM.render(
+      <AppContainer>
+        <Component />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  }
+}
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
+
 registerServiceWorker();
