f0f5824feb73b2d52a4e1a7df7083daf311dcf9c
memotail <memotail@163.com>
2017-11-06 11:20:50 +0800

    custom-router

--- src/routes/Layout/FrameRoutes.js
+++ src/routes/Layout/FrameRoutes.js
@@ -7,13 +7,16 @@ import {
 
 import AsyncComponent from './../../components/AsyncComponent';
 
+const Movie = AsyncComponent(() => import('./../Movie'/* webpackChunkName: "movie" */));
+const Book = AsyncComponent(() => import('./../Book'/* webpackChunkName: "book" */));
+
 // Frames子路有
 export default ({ routeProps }) => {
   return (
     <Switch>
-      <Route exact path="/movie" component={AsyncComponent(() => import('./../Movie'/* webpackChunkName: "movie" */))} />
-      <Route exact path="/book" component={AsyncComponent(() => import('./../Book'/* webpackChunkName: "book" */))} />
-      <Redirect to='/404' />
+      <Route exact path="/movie" component={ Movie } />
+      <Route exact path="/book" component={ Book } />
+      <Redirect to="/404" />
     </Switch>
   );
 };
