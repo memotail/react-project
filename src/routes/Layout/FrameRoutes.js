721aadba19802473c3479334d71707ee57c104e1
memotail <memotail@163.com>
2017-11-06 11:51:18 +0800

    async-reducer

--- src/routes/Layout/FrameRoutes.js
+++ src/routes/Layout/FrameRoutes.js
@@ -8,24 +8,28 @@ import {
 import AsyncComponent from './../../components/AsyncComponent';
 import { injectReducer } from './../../reducers';
 
+const Movie = AsyncComponent(() =>
+  Promise.all([
+    import('./../Movie/Index'/* webpackChunkName: "movie" */),
+    import('./../Movie/Index/reducers'/* webpackChunkName: "movieReducers" */)
+  ]).then(([component, reducer]) => {
+    // 异步加载完模块时候，注入reducers
+    injectReducer('movie', reducer.default);
+
+    // 返回component，AsyncComponent异步后，需要渲染component
+    return component;
+  })
+);
+
+const Book = AsyncComponent(() => import('./../Book'/* webpackChunkName: "book" */));
+
 // Frames子路有
 export default ({ routeProps }) => {
   return (
     <Switch>
-      <Route exact path="/movie" component={AsyncComponent(() =>
-        Promise.all([
-          import('./../Movie/Index'/* webpackChunkName: "movie" */),
-          import('./../Movie/Index/reducers'/* webpackChunkName: "movieReducers" */)
-        ]).then(([component, reducer]) => {
-          // 异步加载完模块时候，注入reducers
-          injectReducer('movie', reducer.default);
-
-          // 返回component，AsyncComponent异步后，需要渲染component
-          return component;
-        })
-      )} />
-      <Route exact path="/book" component={AsyncComponent(() => import('./../Book'/* webpackChunkName: "book" */))} />
-      <Redirect to='/404' />
+      <Route exact path="/movie" component={ Movie } />
+      <Route exact path="/book" component={ Book } />
+      <Redirect to="/404" />
     </Switch>
   );
 };
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -9,28 +9,25 @@ import ApplyedRoute from './../components/route/ApplyedRoute';
 import AuthRoute from './../components/route/AuthRoute';
 import UnAuthRoute from './../components/route/UnAuthRoute';
 
-export default ({ routeProps }) => {
-  const AsnycFrame = AsyncComponent(() => import('./Layout/Frame'/* webpackChunkName: "layoutFrame" */));
+const AsnycFrame = AsyncComponent(() => import('./Layout/Frame'/* webpackChunkName: "layoutFrame" */));
+const Login = AsyncComponent(() => import('./Auth/Login/index' /* webpackChunkName: "login" */));
 
+export default ({ routeProps }) => {
   return (
     <Switch>
-      <Redirect exact from='/' to='/movie'/>
+      <Redirect exact from="/" to="/movie" />
 
       {/* not found */}
       <ApplyedRoute path="/404" props={ routeProps } component={ NotFound } />
 
       {/* auth */}
-      <UnAuthRoute
-       path='/login'
-       props={ routeProps }
-       component={ AsyncComponent(() => import('./Auth/Login/index' /* webpackChunkName: "login" */)) }
-      />
+      <UnAuthRoute path="/login" props={ routeProps } component={ Login } />
 
       {/* layout frame */}
-      <AuthRoute path='/movie' props={ routeProps } component={ AsnycFrame } />
-      <AuthRoute path='/book' props={ routeProps } component={ AsnycFrame } />
+      <AuthRoute path="/movie" props={ routeProps } component={ AsnycFrame } />
+      <AuthRoute path="/book" props={ routeProps } component={ AsnycFrame } />
 
-      <Redirect from='*' to='/404' />
+      <Redirect from="*" to="/404" />
     </Switch>
   );
 };
